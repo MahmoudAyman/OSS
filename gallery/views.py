52f6c869414717e8c9c1735c3dd26738dcbe50f2
MahmoudAyman <mahmoud.ayman07@gmail.com>
2017-05-14 21:44:26 +0200

    gallery is now dynamic

--- gallery/views.py
+++ gallery/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from gallery.models import *
+from django.urls import reverse
+import os 
+from PIL import Image
 # Create your views here.
 
-def index(request):
-    return render(request, 'gallery/index.html')
+def index (request):
+	li = Projects.objects.all()
+	# try:
+	# 	request.session['member_id']
+	# except KeyError:
+	# 	log=False
+	# else:
+	# 	log=True 
+	context={'courses':li}
+	return render(request, 'gallery/index.html',context)
 
